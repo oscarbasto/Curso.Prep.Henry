e543d7ddf7d0d13c2ac247c6373b329747267493
unknown <oscarbasto5627@gmail.com>
2021-11-10 17:08:50 -0600

    Desarrollo v 1.1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -70,8 +70,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x==y){
-           
+  if(x==y){
      return true;
      } 
       else
@@ -79,13 +78,15 @@ function sonIguales(x, y) {
           return false;
         }
   
-     }
+}
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
   
+  
+
 }
 
 function menosQueNoventa(num) {
